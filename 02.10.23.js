2e3992d4c885f62105af46fdd9980772dd23881b
Cycv0 <114578223+Cycv0@users.noreply.github.com>
2023-10-02 11:11:45 +0200

    dd

---
+++ 02.10.23.js
@@ -0,0 +1,18 @@
+
+let mybutton1 = document.getElementById("mybutton1"); 
+let myhead6 = document.getElementById("myhead6"); 
+
+mybutton1.addEventListener("click", function() { 
+  myhead6.innerHTML = "<i>Świetnie!</i>"; 
+});
+
+let mybutton2 = document.getElementById("mybutton2"); 
+let myhead5 = document.getElementById("myhead5"); 
+
+mybutton2.addEventListener("click", function() { 
+  myhead5.innerText = "<i>Świetnie!</i>"; 
+});
+
+let mybutton4 = document.getElementById("mybutton4"); 
+let myparagraph2 = document.getElementById("myparagraph2"); 
+ 
--- console.js
+++ console.js
@@ -1,3 +1,46 @@
-header = document.querySelector('header');
-header.style.backgroundColor= "yellow";
+heder = document.querySelector('header');
+heder.style.backgroundColor= "yellow";
 
+head1 = document.querySelector("header h1");
+document.querySelector("header h1").style.fontFamily = 'Arial Black';
+document.querySelector("header h1").textContent = 'Preambuła';
+
+sections = document.querySelectorAll('section');
+for (const el  of sections) {
+    el.style.border= "2px solid red";
+}
+
+head1 = document.querySelector("section  H1 ");
+console.log(head1);
+head1.textContent = 'Specjalność szefa';
+
+const mainColumn = document.querySelector('.main_column');
+const pElement = document.createElement('p');
+pElement.textContent = 'Kebab na frytkach_-_';
+mainColumn.appendChild(pElement);
+
+const footer1 = document.querySelector('footer');
+console.log(footer1);
+
+const div_element = document.createElement('div');
+div_element.style.backgroundColor = "pink";
+
+const p_element = document.createElement('p');
+p_element.innerText = " &copy";
+
+footer1.appendChild(div_element);
+div_element.appendChild(p_element);
+
+var elements = document.querySelectorAll('h1');
+console.log (elements);
+elements[3].remove();
+var paragrafInnerHTML = document.createElement('p');
+var paragrafInnerText = document.createElement('p');
+
+paragrafInnerHTML.innerHTML = '<i>Pochylony tekst</i>';
+paragrafInnerText.innerText = '<i>Pochylony tekst</i>';
+var header = document.querySelector('header');
+header.appendChild(paragrafInnerHTML);
+header.appendChild(paragrafInnerText);
+console.log('innerHTML:', paragrafInnerHTML.innerHTML);
+console.log('innerText:', paragrafInnerText.innerText);
